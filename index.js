c8e9a88bc6786c8064e42ffaf58e8f37a207f811
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-04 12:18:26 -0400

    update index.js

--- index.js
+++ index.js
@@ -3,11 +3,45 @@ const fs = require('fs');
 const path = require('path');
 const db = require('./db');
 const template = require('./template');
-const config = require('./config.json');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.enable('trust proxy');
+
+app.post('/', (req, res) => {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    if (!req.body.url || req.body.url.length <= 2) {
+        res.status(400).send('Custom slug cannot be less thsan 3 characters');
+    }
+    else if (pattern.test(req.body.url)) {
+        if (req.body.url && (req.body.slug || req.body.slug === '') && !req.body.custom) {
+            db.newLink(req.body).then((slug) => {
+                res.send(slug);
+            }).catch(err => res.status(400).send('Slug in use already'));
+        }
+        else if (req.body.url && (req.body.slug || req.body.slug === '') && req.body.custom) {
+            if (req.body.custom.name || req.body.custom.description || req.body.custom.image || req.body.custom.colour) {
+                db.newLink(req.body).then((slug) => {
+                    res.send(slug);
+                }).catch(err => res.status(400).send('Slug in use already'));
+            }
+            else {
+                res.status(400).send('Invalid request');
+            }
+        }
+        else {
+            res.status(400).send('Invalid request');
+        }
+    }
+    else {
+        res.status(400).send('Not a URL');
+    }
+})
 
 app.use('/', (req, res) => {
     if (req.url === '/') {
@@ -17,7 +51,7 @@ app.use('/', (req, res) => {
     else if (req.url === '/icon.png' || req.url === '/icon.ico') {
         res.sendFile(path.join(__dirname + req.url));
     }
-    else if (req.url.length == config.slug.length) {
+    else if (req.url.length >= 3) {
         db.getLink(req.url.slice(1)).then(row => {
             if (row.redirect == 1) {
                 res.redirect(row.url);
@@ -25,7 +59,7 @@ app.use('/', (req, res) => {
             else {
                 res.send(`${template.render(row)}`);
             }
-        }).catch(err => res.sendStatus(404).end());
+        }).catch(err => { res.sendStatus(404).end() });
     }
     else {
         res.sendStatus(404).end();
