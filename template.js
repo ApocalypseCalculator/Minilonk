0c69de9f4abaf6ba86600a075af7d48d608f196a
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-04-05 17:49:14 -0400

    template.js

--- template.js
+++ template.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
+const pkg = require('./package.json');
 
 
 module.exports.render = (row) => {
     let data = fs.readFileSync('./head.html').toString();
     data = data.replace(/{{REDIRECT}}/g, row.url).replace(/{{NAME}}/g, row.name).replace(/{{DESCRIPTION}}/g, row.description).replace(/{{ICON}}/g, row.image).replace(/{{COLOUR}}/g, row.colour);
     return data;
+}
+
+module.exports.home = () => {
+    return fs.readFileSync('./index.html').toString().replace(/{{VERSION}}/, pkg.version);
 }
